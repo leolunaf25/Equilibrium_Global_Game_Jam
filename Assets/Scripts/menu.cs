4e6c1abf3d58a85ed0ac86cfddd0ba6e03c008ea
leolunaf25 <lunalunaf25@gmail.com>
2022-01-28 15:52:47 -0600

    Add Start Menu

---
+++ Assets/Scripts/menu.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class menu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public void BotonStart()
+    {
+        SceneManager.LoadScene("juego");
+    }
+
+    public void BotonSalir()
+    {
+        Application.Quit();
+    }
+
+    public void BotonInstrucciones()
+    {
+        SceneManager.LoadScene("instrucciones");
+    }
+
+    public void BotonCreditos()
+    {
+        SceneManager.LoadScene("creditos");
+    }
+
+    public void BotonMenu()
+    {
+        SceneManager.LoadScene("menu");
+    }
+
+
+}
